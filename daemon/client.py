575db290c1b6820522dc9359a27ab93b20d702c1
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2019-11-30 08:54:45 +0100

    Make client non-executable

--- daemon/client.py
+++ daemon/client.py
@@ -1,49 +1,30 @@
-import os, sys, argparse, urllib, urllib2, logging
+import os, sys, urllib, urllib2, logging
+
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from parse import parse_cfg, synoindex_modes
 
-## Parse the config
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-config_file = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + '/config.txt'
-cfg = parse_cfg(config_file, "client")
-
-## Setup the client logging file
-client_log = "%s/%s" % (cfg.client_logs, "client.log")
-logging.basicConfig(filename=client_log, filemode='a',
-					format='%(asctime)s - %(levelname)s: %(message)s')
-
-## Set the logger and its level
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
 ### Synoindex-Client
-def client():
+def client(cfg, option, filepath):
 
-	args = argparse.Namespace()
-	parser = argparse.ArgumentParser(description='SynoIndex-Client')
-	parser.add_argument('-o','--option', required=True,
-						help='Option for synoindex')
-	parser.add_argument('-f','--filepath', required=True,
-						help='file path argument for synoindex')
-	args = parser.parse_args()
+	## Setup the client logging file
+	client_log = "%s/%s" % (cfg.client_logs, "client.log")
+	logging.basicConfig(filename=client_log, filemode='a',
+						format='%(asctime)s - %(levelname)s: %(message)s')
 
-	## Read config file
-	config_file = os.path.dirname(os.path.abspath(__file__)) + '/config.txt'
-	cfg = parse_cfg(config_file, "client")
+	## Set the logger and its level
+	logger = logging.getLogger(__name__)
+	logger.setLevel(logging.DEBUG)
 
 	## Validation of the arguments
-	if(args.option not in synoindex_modes):
+	if(option not in synoindex_modes):
 		logger.error("Passed option not supported"); exit()
-	if not os.path.exists(args.filepath):
+	if not os.path.exists(filepath):
 		logger.error("Passed file does not exist"); exit()
 
 	## Call the url and get the answer of the server
-	query_vars = {'option': args.option, 'filepath': args.filepath}
+	query_vars = {'option': option, 'filepath': filepath}
 	url = "%s/synoindex?" % cfg.url
 	url = url + urllib.urlencode(query_vars)
 	logger.debug(url)
 	contents = urllib2.urlopen(url).read()
 	logger.debug(contents)
-
-if __name__ == "__main__":
-	client()
