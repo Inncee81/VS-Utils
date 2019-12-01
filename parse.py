95987737fde2becf229133b501d03fc0d54f545b
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2019-12-01 20:50:32 +0100

    Change indentation

--- parse.py
+++ parse.py
@@ -5,111 +5,115 @@ from collections import namedtuple
 config_types = ["vs-transmission", "vs-handbrake", "vs-synoindex"]
 
 def enum(enum):
-	return enum.replace(" ", "").split(",")
+    return enum.replace(" ", "").split(",")
 
 def get_docker_ip(scope):
-	''' Get the docker0 IP address with or without external packages. '''
+    ''' Get the docker0 IP address with or without external packages. '''
 
-	if scope == "host":
-		import netifaces as ni
-		ni.ifaddresses('docker0')
-		ip = ni.ifaddresses('docker0')[ni.AF_INET][0]['addr']
-	else:
-		import subprocess
-		ps = subprocess.Popen(('ip', 'route', 'show'), stdout=subprocess.PIPE)
-		output = subprocess.check_output(('grep', 'default'), stdin=ps.stdout)
-		ps.wait()
-		ip = re.search(r'([0-9]{1,3}[\.]){3}[0-9]{1,3}', output, flags=0).group()
-	return ip
+    if scope == "host":
+        import netifaces as ni
+        ni.ifaddresses('docker0')
+        ip = ni.ifaddresses('docker0')[ni.AF_INET][0]['addr']
+    else:
+        import subprocess
+        ps = subprocess.Popen(('ip', 'route', 'show'), stdout=subprocess.PIPE)
+        output = subprocess.check_output(('grep', 'default'), stdin=ps.stdout)
+        ps.wait()
+        ip = re.search(r'([0-9]{1,3}[\.]){3}[0-9]{1,3}', output, flags=0).group()
+    return ip
 
 def parse_cfg_transmission(config, sections, fields, scope):
 
-	## Get all mappings from the config and check whether they exist
-	mapping = config._sections['Mapping']
-	mapping = [m for m in list(mapping.items()) if m[0] != "__name__"]
-	for m in mapping:
-		check_path = m[1] if scope == "host" else m[0]
-		if not os.path.exists(check_path):
-			exit("Error: Mapping path doesnt exist: (%s)" % check_path)
+    ## Get all mappings from the config and check whether they exist
+    mapping = config._sections['Mapping']
+    mapping = [m for m in list(mapping.items()) if m[0] != "__name__"]
+    for m in mapping:
+        check_path = m[1] if scope == "host" else m[0]
+        if not os.path.exists(check_path):
+            exit("Error: Mapping path doesnt exist: (%s)" % check_path)
 
-	## Get all handbrake related configs
-	handbrake = config.get(sections[1], fields[1])
-	codecs = enum(config.get(sections[1], fields[2]))
+    ## Get all handbrake related configs
+    handbrake = config.get(sections[1], fields[1])
+    codecs = enum(config.get(sections[1], fields[2]))
 
-	return (mapping, handbrake, codecs)
+    return (mapping, handbrake, codecs)
 
 def parse_cfg_handbrake(config, sections, fields, scope):
 
-	## Get the different episode and movie paths
-	handbrake_movies = enum(config.get(sections[0], fields[0]))
-	handbrake_series = enum(config.get(sections[0], fields[1]))
+    ## Get the different episode and movie paths
+    handbrake_movies = enum(config.get(sections[0], fields[0]))
+    handbrake_series = enum(config.get(sections[0], fields[1]))
 
-	return (handbrake_movies, handbrake_series)
+    return (handbrake_movies, handbrake_series)
 
 def parse_cfg_synoindex(config, sections, fields, scope):
 
-	## Get all server configurations from config file
-	ip = get_docker_ip(scope)
-	port = int(config.get(sections[0], 'server_port'))
-	url = "http://%s:%s" % (ip, port)
+    ## Get all server configurations from config file
+    ip = get_docker_ip(scope)
+    port = int(config.get(sections[0], 'server_port'))
+    url = "http://%s:%s" % (ip, port)
 
-	## Get the debug files
-	server_logs = config.get(sections[0], fields[3])
-	client_logs = config.get(sections[1], fields[4])
+    ## Get the debug files
+    server_logs = config.get(sections[0], fields[3])
+    client_logs = config.get(sections[1], fields[4])
 
-	return (ip, port, url, server_logs, client_logs)
+    return (ip, port, url, server_logs, client_logs)
 
 def parse_cfg(config_file, config_type, scope):
-	''' Parse all configuration options of the config file. '''
+    ''' Parse all configuration options of the config file. '''
 
-	## Check whether the config type is supported
-	if config_type not in config_types:
-		exit("Error: Config type is not supported: %s" % (config_type))
+    ## Check whether the config type is supported
+    if config_type not in config_types:
+        exit("Error: Config type is not supported: %s" % (config_type))
 
-	## Read the config file
-	config = ConfigParser()
-	config.read(config_file)
+    ## Read the config file
+    config = ConfigParser()
+    config.read(config_file)
 
-	## VS-Transmission
-	if (config_type == "vs-transmission"):
-		sections = ['Mapping', 'Handbrake']
-		fields = ["mapping", "handbrake", "codecs"]
+    ## VS-Transmission
+    if (config_type == "vs-transmission"):
+        sections = ['Mapping', 'Handbrake']
+        fields = ["mapping", "handbrake", "codecs"]
 
-	## VS-Handbrake
-	elif (config_type == "vs-handbrake"):
-		sections = ['Handbrake']
-		fields = ["handbrake_movies", "handbrake_series"]
+    ## VS-Handbrake
+    elif (config_type == "vs-handbrake"):
+        sections = ['Handbrake']
+        fields = ["handbrake_movies", "handbrake_series"]
 
-	## VS-SynoIndex
-	elif (config_type == "vs-synoindex"):
-		sections = ["Server", "Client"]
-		fields = ["port", "ip", "url", "server_logs", "client_logs"]
+    ## VS-SynoIndex
+    elif (config_type == "vs-synoindex"):
+        sections = ["Server", "Client"]
+        fields = ["port", "ip", "url", "server_logs", "client_logs"]
 
-	## Check whether all sections are present and initialize config Namespace
-	_ = [exit('Error: Section (%s) missing' % s) for s in sections if not config.has_section(s)]
-	cfg = namedtuple('cfg', " ".join(fields))
-	cfg.__new__.__defaults__ = (None,) * len(cfg._fields)
+    ## Check whether all sections are present and initialize config Namespace
+    _ = [exit('Error: Section (%s) missing' % s) for s in sections if not config.has_section(s)]
+    cfg = namedtuple('cfg', " ".join(fields))
+    cfg.__new__.__defaults__ = (None,) * len(cfg._fields)
 
-	if (config_type == "vs-transmission"):
-		(mapping, handbrake, codecs) = parse_cfg_transmission(config, sections, fields, scope)
-		parsed_cfg = cfg(mapping, handbrake, codecs)
+    if (config_type == "vs-transmission"):
+        (mapping, handbrake, codecs) = parse_cfg_transmission(config, sections, fields, scope)
+        parsed_cfg = cfg(mapping, handbrake, codecs)
 
-	if (config_type == "vs-handbrake"):
-		(handbrake_movies, handbrake_series) = parse_cfg_handbrake(config, sections, fields, scope)
-		parsed_cfg = cfg(handbrake_movies, handbrake_series)
+    if (config_type == "vs-handbrake"):
+        (handbrake_movies, handbrake_series) = parse_cfg_handbrake(config, sections, fields, scope)
+        parsed_cfg = cfg(handbrake_movies, handbrake_series)
 
-	if (config_type == "vs-synoindex"):
-		(ip, port, url, server_logs, client_logs) = parse_cfg_synoindex(config, sections, fields, scope)
-		parsed_cfg = cfg(port, ip, url, server_logs, client_logs)
+    if (config_type == "vs-synoindex"):
+        (ip, port, url, server_logs, client_logs) = parse_cfg_synoindex(config, sections, fields, scope)
+        parsed_cfg = cfg(port, ip, url, server_logs, client_logs)
 
-	return parsed_cfg
+    return parsed_cfg
 
 def parse_dockerpath(mapping, filepath):
-	""" Convert a path within docker container to hostsystem path. """
+    """ Convert a path within docker container to hostsystem path. """
 
-	## General validation
-	if not any(m[0] in filepath for m in mapping): return -1
+    ## General validation
+    if not any(m[0] in filepath for m in mapping):
+        return -1
 
-	## Replace the docker path to the host path
-	for m in mapping: filepath = filepath.replace(m[0], m[1])
-	return (filepath, m[1])
+    ## Replace the docker path to the host path
+    for m in mapping:
+            file_tmp = filepath.replace(m[0], m[1])
+            if file_tmp != filepath:
+                (source_host, root_host) = (file_tmp, m[1])
+    return (source_host, root_host)
--- prints.py
+++ prints.py
@@ -1,24 +1,24 @@
 from datetime import datetime
 
 def printmsg(message, msgtype, arguments=None):
-	cur_date = datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M")
+    cur_date = datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M-%S")
 
-	if arguments:
-		print_str = ", ".join(["%s"] * len(arguments))
-		if msgtype == "error":
-			arguments = (cur_date,) + ("Error: ", ) + (message,) + arguments
-			print(("[%s] %s%s: (" + print_str + ")") % arguments)
-		else:
-			arguments = (cur_date,) + (message,) + arguments
-			print(("[%s] %s: (" + print_str + ")") % arguments)
-	else:
-		if msgtype == "error":
-			print("[%s] %s%s" % (cur_date, "Error: ", message))
-		else:
-			print("[%s] %s" % (cur_date, message))
+    if arguments:
+        print_str = ", ".join(["%s"] * len(arguments))
+        if msgtype == "error":
+            arguments = (cur_date,) + ("Error: ", ) + (message,) + arguments
+            print(("[%s] %s%s: (" + print_str + ")") % arguments)
+        else:
+            arguments = (cur_date,) + (message,) + arguments
+            print(("[%s] %s: (" + print_str + ")") % arguments)
+    else:
+        if msgtype == "error":
+            print("[%s] %s%s" % (cur_date, "Error: ", message))
+        else:
+            print("[%s] %s" % (cur_date, message))
 
 def errmsg(message, arguments=None):
-	printmsg(message, "error", arguments)
+    printmsg(message, "error", arguments)
 
 def debugmsg(message, arguments=None):
-	printmsg(message, "debug", arguments)
+    printmsg(message, "debug", arguments)
