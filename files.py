c6c9771ff31eab35a70947ca191583dd5cf14bd9
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2019-12-01 20:50:06 +0100

    Change permission when copying files

--- files.py
+++ files.py
@@ -1,52 +1,51 @@
-import os, sys, fnmatch, subprocess, errno
+import os, sys, fnmatch, subprocess, errno, stat
 from subprocess import Popen, PIPE, STDOUT, call
 from shutil import copy, copyfile
 
 from prints import errmsg, debugmsg
 
 def files_find_ext(path, ext):
-	''' Find all files in the given path with the extension. '''
+    ''' Find all files in the given path with the extension. '''
 
-	ext_files = []
-	for root, _, filenames in os.walk(path):
-		for filename in fnmatch.filter(filenames, "*.%s" % ext):
-			if ("sample" not in filename):
-				ext_files.append(os.path.join(root, filename))
-	return ext_files
+    ext_files = []
+    for root, _, filenames in os.walk(path):
+        for filename in fnmatch.filter(filenames, "*.%s" % ext):
+            if ("sample" not in filename):
+                ext_files.append(os.path.join(root, filename))
+    return ext_files
 
 def file_copy(src, dst, args):
-	''' Copy file to directory and change owner. '''
+    ''' Copy file to directory and change owner and permissions. '''
 
-	## Without renaming the file
-	file_name = os.path.basename(src)
-	new_file_path = os.path.join(dst, file_name)
-	if not os.path.exists(new_file_path):
-		copy(src, dst)
-		os.chown(new_file_path, args.userid, args.groupid)
-		return dst
-
-	return 0
+    ## Without renaming the file
+    file_name = os.path.basename(src)
+    new_file_path = os.path.join(dst, file_name)
+    copy(src, dst)
+    os.chown(new_file_path, args.userid, args.groupid)
+    mode = stat.S_IRWXU | stat.S_IROTH | stat.S_IRGRP
+    os.chmod(new_file_path, mode)
+    return new_file_path
 
 def file_copy_args(dst, args):
-	''' Copy file to directory and change owner. '''
+    ''' Copy file to directory and change owner. '''
 
-	## Copy the video file to the specified destination
-	new_file = file_copy(args.directory, dst, args)
-	if not new_file:
-		print("Error: Could not copy file (%s) to destination (%s)" % (args.directory, dst))
-		return 0
+    ## Copy the video file to the specified destination
+    new_file = file_copy(args.directory, dst, args)
+    if not new_file:
+        print("Error: Could not copy file (%s) to destination (%s)" % (args.directory, dst))
+        return 0
 
-	return new_file
+    return new_file
 
 def directory_create_owner(args):
-	''' Create a directory by the file name and change owner. '''
+    ''' Create a directory by the file name and change owner. '''
 
-	dir_name = ".".join(args.name.split(".")[:-1])
-	new_dir_path = os.path.join(args.directory, dir_name)
-	if not os.path.exists(new_dir_path):
-		os.mkdir(new_dir_path)
-		os.chown(new_dir_path, args.userid, args.groupid)
-	return new_dir_path
+    dir_name = ".".join(args.name.split(".")[:-1])
+    new_dir_path = os.path.join(args.directory, dir_name)
+    if not os.path.exists(new_dir_path):
+        os.mkdir(new_dir_path)
+        os.chown(new_dir_path, args.userid, args.groupid)
+    return new_dir_path
 
 def crate_path_directories(path):
     try:
@@ -58,16 +57,17 @@ def crate_path_directories(path):
             raise
 
 def unrar_files(abs_path):
-	""" Unzip rar files (synochronous)
+    """ Unzip rar files (synochronous)
 
-	Arguments:
-		abs_path {string} -- Path to the directory containg rar files
-	"""
+    Arguments:
+        abs_path {string} -- Path to the directory containg rar files
+    """
 
-	rar_files = files_find_ext(abs_path, "rar")
-	if rar_files:
-		debugmsg("Found some rar files, try to unrar them")
-		for rar_file in rar_files:
-			process = Popen(["unrar", "x", "-o+", rar_file, abs_path], stdout=PIPE, stderr=PIPE)
-			stderr = process.communicate()[1]
-			print(stderr)
+    rar_files = files_find_ext(abs_path, "rar")
+    if rar_files:
+        debugmsg("Found some rar files, try to unrar them")
+        for rar_file in rar_files:
+            process = Popen(["unrar", "x", "-o+", rar_file, abs_path], stdout=PIPE, stderr=PIPE)
+            stderr = process.communicate()[1]
+            if len(stderr) > 1:
+                print(stderr)
