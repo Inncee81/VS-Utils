18f633686ae839c2a424b4aa4976a4a6ac7a83b0
heimdall-syno <63456651+heimdall-syno@users.noreply.github.com>
2019-12-23 21:07:44 +0100

    Logging file per application
    Instead of dumping to docker logging a seperate logging file per application is created

--- prints.py
+++ prints.py
@@ -1,38 +1,54 @@
-import sys
+import os, sys, errno, logging
 from datetime import datetime
 
-class Logger(object):
-    def __init__(self):
-        self.terminal = sys.stdout
-        self.log = open("/var/log/docker.log", "w")
+logger = ""
 
-    def write(self, message):
-        self.terminal.write(message)
-        self.log.write(message)
+def init_logging():
+    """ Initialize the logging """
 
-    def flush(self):
-        pass
+    ## Check whether the data mapping exists
+    global logger
+    data_dir = os.path.join(os.sep, "data")
+    if not os.path.isdir(data_dir):
+        errmsg("data mount is not available, add it to docker mapping")
+        exit()
 
-def printmsg(message, msgtype, arguments=None, redirect=True):
+    ## Check whether the logging directory exist otherwise create it
+    logs_dir = os.path.join(data_dir, "logs")
+    if  not os.path.isdir(logs_dir):
+        os.mkdir(logs_dir)
+
+    ## Setup the logging files
+    log_file = os.path.join(logs_dir, "logging.log")
+    if not os.path.isfile(log_file): open(log_file, 'a').close()
+
+    ## Setup the logging format
+    logging.basicConfig(filename=log_file, filemode='a', '%(message)s')
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+def printmsg(message, msgtype, arguments=None):
+
+    global logger
     cur_date = datetime.strftime(datetime.now(), "%Y-%m-%d-%H-%M-%S")
-
-    ## Redirect stdout and stderr for docker logs if necessary
-    if redirect:
-        sys.stdout, sys.stderr = (Logger() for _ in range(2))
 
     if arguments:
         print_str = ", ".join(["%s"] * len(arguments))
         if msgtype == "error":
             arguments = (cur_date,) + ("Error: ", ) + (message,) + arguments
             print(("[%s] %s%s: (" + print_str + ")") % arguments)
+            logger.error(("[%s] %s%s: (" + print_str + ")") % arguments)
         else:
             arguments = (cur_date,) + (message,) + arguments
             print(("[%s] %s: (" + print_str + ")") % arguments)
+            logger.info(("[%s] %s: (" + print_str + ")") % arguments)
     else:
         if msgtype == "error":
             print("[%s] %s%s" % (cur_date, "Error: ", message))
+            logger.error("[%s] %s%s" % (cur_date, "Error: ", message))
         else:
             print("[%s] %s" % (cur_date, message))
+            logger.info("[%s] %s" % (cur_date, message))
 
 def errmsg(message, arguments=None):
     printmsg(message, "error", arguments)
